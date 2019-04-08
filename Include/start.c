7c982b91ba43d73c794e379581d00e22028154cc
Stefano Perenzoni <stefano.perenzoni@unitn.it>
2019-04-08 20:57:03 +0200

    Aggiunto comando clear, rimosso \n a fine comando

--- Include/start.c
+++ Include/start.c
@@ -2,26 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define EXIT_CMD "exit\n"
+#define EXIT_CMD "exit"
+#define CLEAR_CMD "clear"
 
 int processCmd(char *command);
 
 
 int start(){
-    char command[60];
+    char *command;
+    ssize_t bufS = 0;
     int status =1;
+
+    //Continuo ad ascoltare in input su stdin
     do{
         printf("Inserisci il comando:>");
-        fgets(command, 60, stdin);
+        //Prendo il comando dall'utente
+        getline(&command, &bufS, stdin);
+        command = strtok(command, "\n");
 
+        //Esegue il comando
         status = processCmd(command);
     }while(status);
 }
 
 int processCmd(char *command){
+    //Esco se cmd "exit"
     if(strcmp(command, EXIT_CMD)==0) {
-        printf("ENTRATO");
         return 0;
+    }
+
+    //Pulisco terminale se cmd "clear"
+    if(strcmp(command, CLEAR_CMD)==0) {
+        system("clear");
+        return 1;
     }
 
     printf("Hai inserito %s", command);
