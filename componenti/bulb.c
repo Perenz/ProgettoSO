7c3883e101bd76a863cc25c9ba9a514a15219d55
Stefano Perenzoni <stefano.perenzoni@unitn.it>
2019-04-11 11:50:53 +0200

    Tentativi di sincronizzazione tra child bulb e parent cen

--- componenti/bulb.c
+++ componenti/bulb.c
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
 
 int main(char **args){
     int id=getpid();
+    int idPar = getppid();
 
     printf("Lampadina creata\n");
-    printf("Pid: %d\nPid padre: %d\n", getpid(), getppid());
-
-    while(1);
+    printf("Pid: %d\nPid padre: %d\n", id, idPar);
     
-
+    //Invio segnale al padre
+    kill(idPar, SIGCONT);
+    
 
     exit(0);
 }
