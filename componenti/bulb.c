5ed443bdbe99798946e3de29a30a45aa93a66eb2
Stefano Perenzoni <stefano.perenzoni@unitn.it>
2019-04-12 15:59:59 +0200

    Sincronizzazione tra child bulb e parent fatta tramite strutture metodi non visti a lezione ma su soluzione online, vedere commenti

--- componenti/bulb.c
+++ componenti/bulb.c
@@ -2,18 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <signal.h>
 
+
+
 int main(char **args){
-    int id=getpid();
-    int idPar = getppid();
+    pid_t id=getpid();
+    pid_t idPar = getppid();
 
     printf("Lampadina creata\n");
     printf("Pid: %d\nPid padre: %d\n", id, idPar);
     
     //Invio segnale al padre
-    kill(idPar, SIGCONT);
-    
+    int ris = kill(idPar, SIGCONT);
 
+    pause();
+
+    printf("child è in pausa");
+    
     exit(0);
 }
+
+
