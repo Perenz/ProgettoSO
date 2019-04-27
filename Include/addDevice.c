805fd4379cc0365ecefb65e377aaa2459ae76ade
Stefano Perenzoni <stefano.perenzoni@unitn.it>
2019-04-27 12:42:58 +0200

    Compattato addDevice facendo una singola funzione per tutti i device, aggiunta sincronizzazione tra parent e child tramite signal per attendere l'inizializzazione del figlio

--- Include/addDevice.c
+++ Include/addDevice.c
@@ -3,11 +3,8 @@
 #include <unistd.h>
 #include <signal.h>
 
-int dev_bulb();
-int dev_window();
-int dev_fridge();
-int dev_timer();
-int dev_hub();
+int add_device(char*);
+void sign_handler(int);
 
 //device list
 char *builtin_device[]={
@@ -18,93 +15,43 @@ char *builtin_device[]={
         "hub"
 };
 
-int (*builtin_addDevice[]) () = {
-        &dev_bulb,
-        &dev_window,
-        &dev_fridge,
-        &dev_timer,
-        &dev_hub
+char *bultin_dev_path[]={
+        "./componenti/BULB",
+        "./componenti/WINDOW",
+        "./componenti/FRIDGE",
+        "./componenti/TIMER",
+        "./componenti/HUB"
 };
 
 int device_number(){
         return sizeof(builtin_device)/sizeof(char*);
 }
 
-int dev_bulb(){
-        pid_t pid, wpid;
-
-        pid = fork();
-        if (pid == 0) {
-                // Child process
-                char *args[]={"./componenti/BULB",NULL};
-                execvp(args[0],args);
-        } else if (pid < 0) {
-                // Errore nell'operazione di fork
-                perror("errore fork");
-        } 
-        sleep(2);
-        kill(pid,SIGINT);
-        sleep(2);
-        return 1;
+//Signal handler per gestire l'arrivo di segnali
+void sign_handler(int sig){
+        return;
 }
 
-int dev_window(){
+int add_device(char* execPath){
         pid_t pid, wpid;
 
-        pid = fork();
+        pid=fork();
         if (pid == 0) {
                 // Child process
-                char *args[]={"./componenti/WINDOW",NULL};
+                char *args[]={execPath,NULL};
                 execvp(args[0],args);
         } else if (pid < 0) {
                 // Errore nell'operazione di fork
                 perror("errore fork");
         } 
-        return 1;
-}
+        else{
+                //Parent process
+                //Indico al processo corrente (il padre) di gestire segnali in entrata di tipo SIGCONT con la funzione sign_handler
+                signal(SIGCONT, sign_handler);
 
-int dev_fridge(){
-        pid_t pid, wpid;
-
-        pid = fork();
-        if (pid == 0) {
-                // Child process
-                char *args[]={"./componenti/FRIDGE",NULL};
-                execvp(args[0],args);
-        } else if (pid < 0) {
-                // Errore nell'operazione di fork
-                perror("errore fork");
-        } 
-        return 1;
-}
-
-int dev_timer(){
-        pid_t pid, wpid;
-
-        pid = fork();
-        if (pid == 0) {
-                // Child process
-                char *args[]={"./componenti/TIMER",NULL};
-                execvp(args[0],args);
-        } else if (pid < 0) {
-                // Errore nell'operazione di fork
-                perror("errore fork");
-        } 
-        return 1;
-}
-
-int dev_hub(){
-        pid_t pid, wpid;
-
-        pid = fork();
-        if (pid == 0) {
-                // Child process
-                char *args[]={"./componenti/HUB",NULL};
-                execvp(args[0],args);
-        } else if (pid < 0) {
-                // Errore nell'operazione di fork
-                perror("errore fork");
-        } 
+                //Vado in pausa per permettere al figlio di generarsi
+                pause();
+        }
         
         return 1;
 }
@@ -117,13 +64,12 @@ int cen_add(char **args){
                 printf("Comando 'device' per vedere la lista di quelli disponibili\n");
                 return 1;
         }
-
         //3 devide disponibili: bulb, window, fridge
         else{
             int i=0;
             for(i=0; i<device_number(); i++){
                 if(strcmp(args[1], builtin_device[i])==0)
-                        return builtin_addDevice[i]();
+                        return add_device(bultin_dev_path[i]);
             }
         }
 
