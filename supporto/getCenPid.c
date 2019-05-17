51c13ebe57d9afad745c7bd61c63933d9478c60f
Perenz <stefanoperenzoni@DESKTOP-Q32JQUR.localdomain>
2019-05-17 15:57:52 +0200

    Ulteriori errori risolti

--- supporto/getCenPid.c
+++ supporto/getCenPid.c
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <errno.h>
 #include <unistd.h>
 #include <signal.h>
 #include <stdlib.h>
@@ -24,25 +25,39 @@ int main(){
 
     signal(SIGQUIT, signhandle_quit);
     //Creo la fifo
-    mkfifo(myFIFO, 0666);
+    if(mkfifo(myFIFO, 0666)<0){
+        fprintf(stderr, "Errore nella creazione della fifo %s: %s\n", myFIFO, strerror(errno));
+    }
+    
 
     while(1){
         //La apro per read only
         fd = open(myFIFO, O_RDONLY);
+        if(fd<0){
+            fprintf(stderr, "Errore nell'apertura READ ONLY della fifo %s: %s\n", myFIFO, strerror(errno));
+        }
 
         //Leggo dalla fifo
-        read(fd, msg, 10);
+        if(read(fd, msg, 10)<0){
+            fprintf(stderr, "Errore nella lettura dalla fifo %s: %s\n", myFIFO, strerror(errno));     
+        }
         if(strcmp(msg, "hand")==0){
             //RISPONDO con il PID della cen
             //Chiudo la fifo in read only
             close(fd);
             //Apro in scrittura
             fd = open(myFIFO, O_WRONLY);
+            if(fd<0){
+                fprintf(stderr, "Errore nell'apertura WRITE ONLY della fifo %s: %s\n", myFIFO, strerror(errno));
+            }
 
             //Rispondo con il pid della centralina
             //Cioe il ppid di tale processo
             sprintf(msg, "%d", cenPid);
-            write(fd, msg, strlen(msg)+1);
+            if(write(fd, msg, strlen(msg)+1)<0){
+                fprintf(stderr, "Errore nella scrittura sulla fifo %s: %s\n", myFIFO, strerror(errno));     
+
+            }
             //Chiudo in scrittura
             close(fd);
         }
